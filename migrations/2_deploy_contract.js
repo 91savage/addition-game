8e2c1cd5d4953a39d153d727915cc5b8a6fae5e6
91savage <ekdma1403@gmail.com>
2022-06-21 13:48:28 +0900

    deploy complete

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,25 +1,26 @@
-const fs = require('fs') // filesystem /s/
+const fs = require('fs') // filesystem
 const AdditionGame = artifacts.require('./AdditionGame.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(AdditionGame)
   .then(() => {
     if(AdditionGame._json) {
-      fs.writeFile('deployedABI', JSON.stringify(AdditionGame._json.abi)),
+      fs.writeFile('deployedABI', JSON.stringify(AdditionGame._json.abi),
       (err) => {
         if (err) throw err;
         console.log("파일에 ABI 입력 성공");
       }
-    }
+      )
 
     fs.writeFile('deployAddress', AdditionGame.address,
     (err) => {
       if(err) throw err;
       console.log("파일에 주소 입력 성공");
-      
     }
-    
     )
+  }
   })
 }
+
+
 
---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
+const NETWORK_ID = "1001";
+const GASLIMIT = "20000000";
+const URL = `https://api.baobab.klaytn.net:8651`;
+const PRIVATE_KEY = "0xd25293075580320fdbb666d5ba65f5a2870cef3055cfd8eb4076f4b95766e1d7";
+
+module.exports = {
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
+
+    klaytn: {
+      provider: new HDWalletProvider(PRIVATE_KEY, URL),
+      network_id: NETWORK_ID,
+      gas: GASLIMIT,
+      gasPrice: null,
+    },
+  },
+};
+
+
+
