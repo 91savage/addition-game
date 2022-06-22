90d0921a686675bb1721954f25f55515eb141d7b
91savage <ekdma1403@gmail.com>
2022-06-23 03:24:02 +0900

    06/23

--- src/index.js
+++ src/index.js
@@ -40,9 +40,16 @@ const App = {
     this.auth.password = event.target.value;
   },
 
-  handleLogin: async function () {
-    if (this.auth.accesType === 'keystore') {
-      
+  handleLogin: async function () { // keystore과 비밀번호를 통해서 얻을 수 있는 것 : private key
+    if (this.auth.accesType === 'keystore') { //나중에 프라이빗 키를 넣어야 할 때는 변경하여 사용
+      try {
+        const privatekey = cav.klay.accounts.decrypt(this.auth.keystore, this.auth.password).privatekey; 
+        // cav 인스턴스의 accounts member를통해서 decrypts(해독) 함수를 쓸 수 있음 (keystore파일 내용과 비밀번호를 인자로 넘겨서 derypt된 계정 오브젝트를 반환받음
+        // 그 오브젝트 중에서 privatekey를 가져와서 상수에다가 저장시킴
+        this.integrateWallet(privatekey);
+      } catch (e) {
+        $('#message').text('비밀번호가 일치하지 않습니다.');
+      }
     }
   },
 
@@ -85,8 +92,11 @@ const App = {
 
   },
 
-  integrateWallet: function (privateKey) {
-
+  integrateWallet: function (privateKey) { //인자로 받은 프라이빗 키로 월렛 인스턴스를 가져옴 / 계정인증완료
+    const walletInstance = cav.klay.accounts.privateKeyToAccount(privateKey)  
+    cav.klay.accounts.wallet.add(walletInstance)// 앞으로 트랜잭션을 사용 할 때 내 계정정보를 쉽게 불러와서 사용 할 수 있음.
+    sessionStorage.setItem('walletInstance', JSON.stringify(walletInstance));//세션에 월렛 인스턴스 저장 / setItem은 wallet 인스턴스를 키값으로 불러옴, 쌍으로 같이 저장된 value값이 같이 불러와짐
+    this.changeUI(walletInstance);
   },
 
   reset: function () {
@@ -94,7 +104,10 @@ const App = {
   },
 
   changeUI: async function (walletInstance) {
-
+    $('#loginModal').modal('hide');
+    $('#login').hide();
+    $('#login').show();
+    $('#address').append('<br>' + '<p>' + '내 계정 주소: ' + walletInstance.address + '</p>');
   },
 
   removeWallet: function () {
