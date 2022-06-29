f6e988cdbc1ea3074297d3caa2c9527e79d02f96
91savage <ekdma1403@gmail.com>
2022-06-29 19:42:10 +0900

    ttt

---
+++ test2.js
@@ -0,0 +1,12 @@
+function solution(arr1, arr2) {
+    let answer = [];
+
+    if (answer) {
+        console.log("abc");
+    }else {
+        asldkajsdlksdad
+
+    } 
+
+    console.log("sdsd");
+    
